b56bd6e45b0d0742c4fea9ee2bf1b239d223cfef
talzateg-netizen <talzateg@gmail.com>
2026-02-07 17:37:09 -0500

    inicialización

--- src/main/java/com/actividad/Main.java
+++ src/main/java/com/actividad/Main.java
@@ -3,7 +3,9 @@ package com.actividad;
 public class Main {
     public static void main(String[] args) {
       
-        int 23
+       
 
     }
+
+
 }
---
+++ src/main/java/com/actividad/auto.java
@@ -0,0 +1,21 @@
+
+package com.actividad;
+
+
+public class Auto {
+
+    String marca = "Audi";
+    String color = "negro";
+    int cilindraje = 5;
+    
+
+public MostrarInformación ( String marca, String color, int cilindraje {
+    
+ System.out.println( marca + color + cilindraje )
+
+}
+
+   
+)
+
+}
